664167d75c001d28912a26d72ac91ab15639a374
gjerokrsteski <gjero@krsteski.de>
2014-01-10 13:46:51 +0100

    fix system config over loading

--- app/bootstrap.app.php
+++ app/bootstrap.app.php
@@ -6,8 +6,9 @@
 */
 $root = dirname(dirname(__FILE__));
 
-require_once $root.'/pimf-framework/config.core.php';
-require_once $root.'/app/config.app.php';
+include_once $root.'/pimf-framework/config.core.php';
+include_once $root.'/app/config.app.php';
+
 require_once $root.'/pimf-framework/autoload.core.php';
 require_once $root.'/app/autoload.app.php';
 require_once $root.'/pimf-framework/utils.php';
